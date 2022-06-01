e3f012cc06d0a6f7bbd7be093cb641e5cd9a8f5f
srz-deepthi <deepthik@smartrabbitz.com>
2022-06-01 17:32:18 +0530

    With edit button

--- hspt-management/src/components/reports.js
+++ hspt-management/src/components/reports.js
@@ -1,12 +1,27 @@
 import React from 'react'
 import Table from './table'
 
-export const Button = () => {
+export const Button = (props) => {
+  //  const handleChange = (event) => {
+  //    alert("Edit")
+  //  }
     return(
-      <button>Edit</button>
+      <button onClick={ props.handleClick }>
+        <svg fill="none" viewBox="0 0 24 24" stroke="#0f2c69" strokeWidth={2} width="20">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+        </svg>
+      </button>
     )
 }
 
+const handleClick = (data) => () => {
+  console.log("Button Clicked",data)
+}
+const getContent = (rawData) => 
+        rawData.map(item => (
+          {...item, edit: <Button handleClick={handleClick(item)}/>}
+        ))
+      
 const Reports = () => {
     const [appointmentList] = React.useState({
         headingLabel:[
@@ -15,43 +30,49 @@ const Reports = () => {
             { label:'Specialist', keyValue:'specialist' },
             { label:'Speciality', keyValue:'speciality' },
             { label:'Reports', keyValue:'reports' },
-            { label:'Edit'}
+            { label:'Edit', keyValue:'edit' }
     ],
-        // , 'Specialist', 'Speciality', 'Reports'],
         patientAppointmentsList:[
           {
             token:'23011',
             visit_date:'22-01-2021',
             specialist:'Dr.Andrew',
             speciality:'Cardiology',
-            reports:'Reports'
+            reports:'Reports',
+            // edit: <Button/>
           },
           {
             token:'23012',
             visit_date:'23-05-2022',
             specialist:'Dr.Pete',
             speciality:'Nuerology',
-            reports:'Reports'
+            reports:'Reports',
+            // edit: <Button/>
           },
           {
             token:'23013',
             visit_date:'25-05-2022',
             specialist:'Dr.Samuel',
             speciality:'Surgery',
-            reports:'Reports'
+            reports:'Reports',
+            // edit: <Button/>
+
           },
           {
             token:'23014',
             visit_date:'28-05-2022',
             specialist:'Dr.Andrew',
             speciality:'Cardiology',
-            reports:'Reports'
+            reports:'Reports',
+            // edit: <Button/>
+
           },{
             token:'23015',
             visit_date:'30-05-2022',
             specialist:'Dr.Issac',
             speciality:'ENT',
-            reports:'Reports'
+            reports:'Reports',
+            // edit: <Button/>
           }
         ]
     })
@@ -60,7 +81,8 @@ const Reports = () => {
         <Table 
                 label="Patient's Appointment Record" 
                 head={appointmentList.headingLabel} 
-                data={appointmentList.patientAppointmentsList}
+                data={getContent(appointmentList.patientAppointmentsList)}
+
         />
     </div>
   )
